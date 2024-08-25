47752718da76ecc01bd036a5082cae014b608ed6
garosoro <gabrielr.sotorojas@gmail.com>
2024-08-25 18:04:00 -0500

    refactor: use powershell directives

--- webp-no-resize - create cbr.ps1
+++ webp-no-resize - create cbr.ps1
@@ -1,55 +1,54 @@
 $fileNames = Get-ChildItem -Path $scriptPath -Recurse -Include *.cbz, *.cbr
 
 foreach ($f in $fileNames) {
-    echo "$($f.Directory)\$($f.Basename)$($f.Extension)"
-    #switch ($f.Extension)
-    #{
-    #    '.cbz' {
-    #        7za x "$($f.Directory)\$($f.Basename).cbz"
-    #    }
-    #    '.cbr' {
-    #        unrar x "$($f.Directory)\$($f.Basename).cbr"
-    #    }
-    #}
+    Write-Output "$($f.Directory)\$($f.Basename)$($f.Extension)"
+    switch ($f.Extension) {
+        '.cbz' {
+            7za x "$($f.Directory)\$($f.Basename).cbz"
+        }
+        '.cbr' {
+            unrar x "$($f.Directory)\$($f.Basename).cbr"
+        }
+    }
     #cwebp "$($f)" -o "$($f.Directory)\$($f.Basename).webp"
     #del /S *.jpg
     #del /S *.png
 
     #rar a D:\Biblioteca\Manga\"Black Clover"\"Black Clover - Tomo 26".cbr  D:\Biblioteca\Manga\"Black Clover"\"Tomo 26"
 
-    #echo $f
+    #Write-Output $f
     # New-Item "$($f).txt"
     # cwebp "$($f)" -o "$($f.Directory)\$($f.Basename).webp"
     #rar a D:\Biblioteca\Manga\"Black Clover"\"Black Clover - Tomo 26".cbr  D:\Biblioteca\Manga\"Black Clover"\"Tomo 26"
     #7za x *.cbz
     #unrar x *.cbz
 }
-echo "--------------------------------------------------"
-echo "--------------------------------------------------"
+Write-Output "--------------------------------------------------"
+Write-Output "--------------------------------------------------"
 $imageNames = Get-ChildItem -Path $scriptPath -Recurse -Include *.png, *.jpg
 foreach ($f in $imageNames) {
-    echo $f
-    echo "$($f)"
-    echo "$($f.Directory)\$($f.Basename).webp"
-    #cwebp "$($f)" -o "$($f.Directory)\$($f.Basename).webp"
+    #Write-Output $f
+    #Write-Output "$($f)"
+    #Write-Output "$($f.Directory)\$($f.Basename).webp"
+    cwebp "$($f)" -o "$($f.Directory)\$($f.Basename).webp"
     
 }
-echo "--------------------------------------------------"
-echo "--------------------------------------------------"
+Write-Output "--------------------------------------------------"
+Write-Output "--------------------------------------------------"
 
 Remove-Item * -Recurse -Include *.png, *.jpg
-echo "Imagenes .jpg y .png borradas"
+Write-Output "Imagenes .jpg y .png borradas"
 
-echo "--------------------------------------------------"
-echo "--------------------------------------------------"
+Write-Output "--------------------------------------------------"
+Write-Output "--------------------------------------------------"
 
 $folderNames = Get-ChildItem -Path $scriptPath -Directory
 foreach ($f in $folderNames) {
-    echo $f.FullName
-    #echo "$($f.Directory)\$($f.Basename).cbr"
-    #echo "$($f.Directory)\$($f.Basename)"
+    #Write-Output $f.FullName
+    #Write-Output "$($f.Directory)\$($f.Basename).cbr"
+    #Write-Output "$($f.Directory)\$($f.Basename)"
     #rar a "$($f.Directory)\$($f.Basename).cbr" "$($f.Directory)\$($f.Basename)"
-    rar a "$($f.FullName).cbr" "$($f.FullName)"
+    rar a -m5 "$($f.FullName).cbr" "$($f)"
 }
-echo "--------------------------------------------------"
-echo "--------------------------------------------------"
+Write-Output "--------------------------------------------------"
+Write-Output "--------------------------------------------------"
